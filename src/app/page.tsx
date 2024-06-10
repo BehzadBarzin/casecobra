5bd274a1bf48fadb24b452cc1d4670358bccebb4
Behzad Barzin <behzad_barzin@live.com>
2024-06-10 17:36:34 +0330

    Add Tailwind Prettier plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-    </main>
-  );
+  return <div className="bg-slate-50 p-5 pt-10"></div>;
 }
