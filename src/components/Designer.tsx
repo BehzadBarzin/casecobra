8a86c5e7e9458b5b5f0cc516f325a90d08f2129a
Behzad Barzin <behzad_barzin@live.com>
2024-06-11 23:43:26 +0330

    Adds materials and finishes in Designer

--- src/components/Designer.tsx
+++ src/components/Designer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { cn } from "@/lib/utils";
+import { cn, formatPrice } from "@/lib/utils";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import NextImage from "next/image";
 import React, { FC } from "react";
@@ -216,7 +216,78 @@ const Designer: FC<IProps> = ({ configId, imageUrl, imageDimensions }) => {
                     </DropdownMenuContent>
                   </DropdownMenu>
                 </div>
-                {/* Material------------------------------------------------ */}
+                {/* Material and Finishes----------------------------------- */}
+                {/* Map over both 'materia'l and 'finish' */}
+                {[MATERIALS, FINISHES].map(
+                  ({ name, options: selectableOptions }) => (
+                    // A radio group for either 'material' or 'finish'
+                    <RadioGroup
+                      key={name}
+                      value={options[name]}
+                      onChange={(val) => {
+                        setOptions((prev) => ({
+                          ...prev,
+                          // name: 'material' | 'finish'
+                          [name]: val,
+                        }));
+                      }}
+                    >
+                      {/* Label: 'Material' | 'Finish' */}
+                      <Label>
+                        {name.slice(0, 1).toUpperCase() + name.slice(1)}
+                      </Label>
+                      {/* Options (of either 'material' or 'finish') */}
+                      <div className="mt-3 space-y-4">
+                        {selectableOptions.map((option) => (
+                          <RadioGroup.Option
+                            key={option.value}
+                            value={option}
+                            className={({ active, checked }) =>
+                              cn(
+                                "relative block cursor-pointer rounded-lg border-2 border-zinc-200 bg-white px-6 py-4 shadow-sm outline-none ring-0 focus:outline-none focus:ring-0 sm:flex sm:justify-between",
+                                {
+                                  "border-primary": active || checked,
+                                },
+                              )
+                            }
+                          >
+                            <span className="flex items-center">
+                              <span className="flex flex-col text-sm">
+                                <RadioGroup.Label
+                                  className="font-medium text-gray-900"
+                                  as="span"
+                                >
+                                  {option.label}
+                                </RadioGroup.Label>
+
+                                {option.description ? (
+                                  <RadioGroup.Description
+                                    as="span"
+                                    className="text-gray-500"
+                                  >
+                                    <span className="block sm:inline">
+                                      {option.description}
+                                    </span>
+                                  </RadioGroup.Description>
+                                ) : null}
+                              </span>
+                            </span>
+
+                            <RadioGroup.Description
+                              as="span"
+                              className="mt-2 flex text-sm sm:ml-4 sm:mt-0 sm:flex-col sm:text-right"
+                            >
+                              {/* Display option's price */}
+                              <span className="font-medium text-gray-900">
+                                {formatPrice(option.price / 100)}
+                              </span>
+                            </RadioGroup.Description>
+                          </RadioGroup.Option>
+                        ))}
+                      </div>
+                    </RadioGroup>
+                  ),
+                )}
                 {/* -------------------------------------------------------- */}
               </div>
             </div>
