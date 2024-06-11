5a8ccc0c612ad5b7387bcac5adb7f81ef27aae04
Behzad Barzin <behzad_barzin@live.com>
2024-06-12 03:11:33 +0330

    Setting up Design Preview page

---
+++ src/app/configure/preview/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import DesignPreview from "@/components/DesignPreview";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import React, { FC } from "react";
+
+interface IProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const page: FC<IProps> = async ({ searchParams }) => {
+  // ---------------------------------------------------------------------------
+  // Get config id from search params
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  // ---------------------------------------------------------------------------
+  // Get configuration from database
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+  // ---------------------------------------------------------------------------
+  return <DesignPreview configuration={configuration} />;
+};
+
+export default page;
---
+++ src/components/DesignPreview.tsx
@@ -0,0 +1,12 @@
+import { Configuration } from "@prisma/client";
+import React, { FC } from "react";
+
+interface IProps {
+  configuration: Configuration;
+}
+
+const DesignPreview: FC<IProps> = ({ configuration }) => {
+  return <div>{configuration.id}</div>;
+};
+
+export default DesignPreview;
