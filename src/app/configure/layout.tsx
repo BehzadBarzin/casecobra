ad0996b581ef2d3e0661b123b74f16ed2de6934b
Behzad Barzin <behzad_barzin@live.com>
2024-06-11 18:36:09 +0330

    Implement Steps indicator

--- src/app/configure/layout.tsx
+++ src/app/configure/layout.tsx
@@ -1,9 +1,12 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Steps from "@/components/Steps";
 import { ReactNode } from "react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <MaxWidthWrapper className="flex flex-1 flex-col">
+      <Steps />
+
       {children}
     </MaxWidthWrapper>
   );
