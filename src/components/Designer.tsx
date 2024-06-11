b51b369494cabedce963513fb4f8e5db558fbdfd
Behzad Barzin <behzad_barzin@live.com>
2024-06-11 23:00:03 +0330

    Adds Image configuration to Designer

--- src/components/Designer.tsx
+++ src/components/Designer.tsx
@@ -4,6 +4,8 @@ import { cn } from "@/lib/utils";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
 import NextImage from "next/image";
 import React, { FC } from "react";
+import { Rnd } from "react-rnd";
+import HandleComponent from "./HandleComponent";
 
 interface IProps {
   configId: string;
@@ -16,7 +18,7 @@ const Designer: FC<IProps> = ({ configId, imageUrl, imageDimensions }) => {
   // ---------------------------------------------------------------------------
   return (
     <div className="relative mb-20 mt-20 grid grid-cols-1 pb-20 lg:grid-cols-3">
-      {/* ------------------------------------------------------------------ */}
+      {/* Configure Image--------------------------------------------------- */}
       <div className="relative col-span-2 flex h-[37.5rem] w-full max-w-4xl items-center justify-center overflow-hidden rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
         {/* Phone Template */}
         <div className="pointer-events-none relative aspect-[896/1831] w-60 bg-opacity-50">
@@ -46,19 +48,45 @@ const Designer: FC<IProps> = ({ configId, imageUrl, imageDimensions }) => {
           {/* -------------------------------------------------------------- */}
         </div>
         {/* User's Image---------------------------------------------------- */}
-        {/* RND */}
-        <div className="relative h-full w-full">
-          <NextImage
-            src={imageUrl}
-            fill
-            alt="your image"
-            className="pointer-events-none"
-          />
-        </div>
-        {/* /RND */}
+        {/* RND to enable the user to drag the image around */}
+        <Rnd
+          default={{
+            x: 150,
+            y: 205,
+            height: imageDimensions.height / 4,
+            width: imageDimensions.width / 4,
+          }}
+          onResizeStop={(_, __, ref, ___, { x, y }) => {
+            // Update image
+          }}
+          onDragStop={(_, data) => {
+            const { x, y } = data;
+
+            // Update image
+          }}
+          className="absolute z-20 border-[3px] border-dashed border-primary"
+          lockAspectRatio
+          resizeHandleComponent={{
+            bottomRight: <HandleComponent />,
+            bottomLeft: <HandleComponent />,
+            topRight: <HandleComponent />,
+            topLeft: <HandleComponent />,
+          }}
+        >
+          {/* Image */}
+          <div className="relative h-full w-full">
+            <NextImage
+              src={imageUrl}
+              fill
+              alt="your image"
+              className="pointer-events-none"
+            />
+          </div>
+        </Rnd>
         {/* ---------------------------------------------------------------- */}
       </div>
       {/* ------------------------------------------------------------------ */}
+      {/* Configure the Case------------------------------------------------ */}
       <div></div>
       {/* ------------------------------------------------------------------ */}
     </div>
