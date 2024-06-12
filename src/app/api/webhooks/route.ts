f2d4fd05820937c360da35dcc796c8bd7027238b
Behzad Barzin <behzad_barzin@live.com>
2024-06-12 17:41:48 +0330

    Implemented order success email

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // Stripe Webhook
 export async function POST(req: Request) {
@@ -71,7 +75,25 @@ export async function POST(req: Request) {
         },
       });
 
-      // Todo: Send Email
+      // Send Email to user
+      await resend.emails.send({
+        from: "CaseCobra <hello@joshtriedcoding.com>",
+        to: [event.data.object.customer_details.email],
+        subject: "Thanks for your order!",
+        react: OrderReceivedEmail({
+          orderId,
+          orderDate: updatedOrder.createdAt.toLocaleDateString(),
+          // @ts-ignore
+          shippingAddress: {
+            name: session.customer_details!.name!,
+            city: shippingAddress!.city!,
+            country: shippingAddress!.country!,
+            postalCode: shippingAddress!.postal_code!,
+            street: shippingAddress!.line1!,
+            state: shippingAddress!.state,
+          },
+        }),
+      });
     }
 
     return NextResponse.json({ result: event, ok: true });
